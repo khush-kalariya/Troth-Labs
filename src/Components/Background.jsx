6247a595ecc1bf80f1bdfa9d3c9cdff76f045161
khush-kalariya <khushpatel9532@gmail.com>
2025-04-26 16:00:29 +0530

    third commit

--- src/Components/Background.jsx
+++ src/Components/Background.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 function Background() {
-  return <div className="bg-[#FFE9DC] w-full h-full lg:pt-27 pt-10"></div>;
+  return <div className="bg-[#FFE9DC] w-full h-full lg:pt-27 pt-13"></div>;
 }
 
 export default Background
